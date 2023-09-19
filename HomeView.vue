da118657c122a3702839ce8cb052d9696afe93fe
bttu-ka <141981341+bttu-ka@users.noreply.github.com>
2023-09-19 19:43:05 +1000

    HomeView.vue

--- HomeView.vue
+++ HomeView.vue
@@ -1,56 +1,55 @@
 <template>
   <div class="football-match">
-    
+    //Football Match Details
     <h2>Football Match Details</h2>
-    <p>Home Team: {{ homeTeam }}</p> 
+    <p>Home Team: {{ homeTeam }}</p>
     <p>Away Team: {{ awayTeam }}</p>
-    <p v-html="matchDescription"></p> 
+    <p v-html="matchDescription"></p>
 
-    
-    <p :id="venueId"></p> 
+    <!-- 2. Match Venue -->
+    <p :id="venueId"></p>
 
-    
-    <p>Match Date: {{ matchDate }}</p> 
+    <!-- 3. Match Date -->
+    <p>Match Date: {{ matchDate }}</p>
 
-    
-    <button @click="startMatch">Start Match</button> 
+    <!-- 4. Football Match Methods -->
+    <button @click="startMatch">Start Match</button>
 
-    
+    <!-- 5. Match Score -->
     <h2>Match Score</h2>
     <p>Total Goals: {{ totalGoals }}</p>
-    
 
-    
-    <h2>Player on the field </h2>
+    <!-- 6. Players on the Field -->
+    <h2>Players on the Field</h2>
     <ul>
-      <li v-for="player in players" :key="player.id">{{ player.name }}</li> 
+      <li v-for="player in players" :key="player.id">{{ player.name }}</li>
     </ul>
 
-    
-    <button @click="scoreGoal">Score a Goal</button> 
-    
+    <!-- 7. Event Handling: Football Match Events -->
+    <button @click="scoreGoal">Score a Goal</button>
 
-    
+    <!-- 8. Football Match Form -->
     <h2>Enter Match Details</h2>
     <label for="goals">Goals:</label>
-    <input type="number" v-model="goals" /> 
+    <input type="number" v-model.number="goals" /> <!-- Added v-model.number -->
     <br />
-    
     <label for="manOfTheMatch">Man of the Match:</label>
     <select v-model="manOfTheMatch">
       <option value="player1">Messi</option>
       <option value="player2">Ronaldo</option>
-    </select> 
+    </select>
     <br />
     <label for="matchSummary">Match Summary:</label>
-    <textarea v-model="matchSummary"></textarea>
+    <textarea v-model.trim="matchSummary"></textarea> <!-- Added v-model.trim -->
 
-    
+    <!-- 9. Football Match Watchers -->
+    <p>Goals Watcher: {{ goalsWatcher }}</p>
+
+    <!-- 10. Football Match Components -->
     <h2>Football Match Components</h2>
-    <match-summary :summary="matchSummary"></match-summary> 
-    <player-stats @goal="handleGoalEvent"></player-stats> 
     <slot-example>
-     
+      <!-- c. Slots -->
+      <div slot="football-slot">Football Slot Content</div>
     </slot-example>
   </div>
 </template>
@@ -58,45 +57,45 @@
 <script setup>
 import { ref, computed, watch } from 'vue';
 
-//Match Details
+// Match Details
 const homeTeam = ref('Home Team');
 const awayTeam = ref('Away Team');
 const matchDescription = '<p>An exciting football match!</p>';
 const venueId = 'football-stadium';
 const matchDate = '2023-09-15';
 
-//Match Methods
+// 4. Football Match Methods
 const startMatch = () => {
   alert('Match has started!');
 };
 
-//Match Score
+// 5. Match Score
 const totalGoals = ref(0);
 
-//Player List
+// 6. Player List
 const players = ref([
   { id: 1, name: 'Messi' },
   { id: 2, name: 'Ronaldo' },
   { id: 3, name: 'Mbappe' },
 ]);
 
-// Event Handling
+// 7. Event Handling: Football Match Events 
 const scoreGoal = () => {
   totalGoals.value++;
 };
 
-
-
-//Football Match Form
+// Football Match Form
 const goals = ref(0);
 const manOfTheMatch = ref('');
 const matchSummary = ref('');
 
+// 9. Football Match Watchers
+const goalsWatcher = computed(() => {
+  return goals.value > 5 ? 'High scoring match!' : 'Normal match.';
+});
 
-
-
+// 10. Football Match Components (Not shown here)
 const handleGoalEvent = (data) => {
-
 };
 </script>
 
